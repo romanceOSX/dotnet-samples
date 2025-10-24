09ca821273a0de5d9e15ea03094e71274e04d239
romance <106858958+romanceOSX@users.noreply.github.com>
2025-10-23 22:24:05 -0600

    chore: sample project

---
+++ csharp_sample_cli/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HelloWorld
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+
+            var names = new [] {"Abc", "SomeD", "SomeC"};
+            foreach (var name in names) {
+                Console.WriteLine($"Hello {name}");
+            }
+        }
+    }   
+}
+
