a00cb9da28dde695bd4b35abcd3dc73f287dbb49
romance <106858958+romanceOSX@users.noreply.github.com>
2025-10-25 10:35:02 -0600

    chore: delegates

---
+++ foo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace  MyApp;
+
+delegate void MyDelegate(int a, int b);
+
+class Application {
+    public static void Foo1(int a, int b) {
+        Console.WriteLine($"Calling from Foo1 {a}, {b}");
+    }
+    public static void Foo2(int a, int b) {
+        Console.WriteLine($"Calling from Foo2 {a}, {b}");
+    }
+
+    public static void testDelegates() {
+        MyDelegate del = new MyDelegate(Foo1);
+        del += Foo2;
+
+        del.Invoke(1, 2);
+    }
+
+    public static void Main(string[] args) {
+        Console.WriteLine("-- Testing Delegates");
+        testDelegates();
+    }
+}
+
