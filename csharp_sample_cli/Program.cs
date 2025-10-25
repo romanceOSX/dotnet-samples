7b601cb222a3cbb1e6763f6828d7a0037327bb6c
romance <106858958+romanceOSX@users.noreply.github.com>
2025-10-24 22:07:51 -0600

    chore: updated program

--- csharp_sample_cli/Program.cs
+++ csharp_sample_cli/Program.cs
@@ -9,6 +9,24 @@ namespace HelloWorld
             foreach (var name in names) {
                 Console.WriteLine($"Hello {name}");
             }
+
+            //
+            // strings
+            //
+
+            // string length
+            string sample_name = "Juan";
+            Console.WriteLine($"This is the name: {sample_name}, whose length is: {sample_name.Length}");
+            
+            // trimmings
+            sample_name = "     Some other naamee      ";
+            Console.WriteLine($"original: [{sample_name}]");
+            sample_name = sample_name.TrimEnd();
+            Console.WriteLine($"[{sample_name}]");
+            sample_name = sample_name.TrimStart();
+            Console.WriteLine($"[{sample_name}]");
+            sample_name = sample_name.Trim();
+            Console.WriteLine($"[{sample_name}]");
         }
     }   
 }
